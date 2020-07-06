86719354da20c824e4e2ea6dd7f3e655cd82c89a
felipe98 <angelfelipe1398@gmail.com>
2020-07-05 19:04:31 -0500

    Preparacion para produccion

--- CloudPython/CloudPython/settings.py
+++ CloudPython/CloudPython/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +138,8 @@ EMAIL_HOST_USER = 'founde.me@gmail.com'
 EMAIL_HOST_PASSWORD = 'angelFOUNDME123+'
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
+
+
+############ Heroku ############
+
+django_heroku.settings(locals())
